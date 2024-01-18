57178f95a46ec96e2871176d9a21339a76f04257
GitClassSara <156311636+GitClassSara@users.noreply.github.com>
2024-01-18 14:10:33 +0100

    Add files via upload
    Actividad 1 Unidad 4

---
+++ Model/Atributos.java
@@ -0,0 +1,53 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Atributos {
+
+    /**
+     * Default constructor
+     */
+    public Atributos() {
+    }
+
+    /**
+     * 
+     */
+    public void nombrePersona;
+
+    /**
+     * 
+     */
+    public String provincia = "Zaragoza";
+
+    /**
+     * 
+     */
+    Punto origen;
+
+    /**
+     * 
+     */
+    static Long idUnico;
+
+    /**
+     * 
+     */
+    private Integer segundoApellido;
+
+    /**
+     * 
+     */
+    protected Entero prioridad = 1;
+
+    /**
+     * 
+     */
+    public void Operation1() {
+        // TODO implement here
+    }
+
+}
